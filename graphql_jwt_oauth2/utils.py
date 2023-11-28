74675cc23db216491b1f7152fc15331da56f3692
advl <sonora@779.mx>
2023-11-28 18:07:54 +0000

    Added the timestamp transfer property

--- graphql_jwt_oauth2/utils.py
+++ graphql_jwt_oauth2/utils.py
@@ -18,27 +18,40 @@ Variables:
 """
 
 from datetime import datetime, timedelta
-from typing import Any, Optional, Tuple
+from typing import Any, Dict
+from urllib.parse import parse_qs, urlencode, urlsplit, urlunsplit
 
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from graphql_jwt.refresh_token.shortcuts import create_refresh_token, get_refresh_token
 from graphql_jwt.settings import jwt_settings
 from graphql_jwt.utils import jwt_encode, jwt_payload, set_cookie
 
 
-def calculate_expiration(delta: timedelta) -> datetime:
+def append_query_params(
+    response: HttpResponseRedirect, new_params: Dict[str, str]
+) -> None:
     """
-    Calculate the expiration time based on the provided timedelta.
+    Append additional query parameters to the URL of a HttpResponseRedirect,
+    mutating it in place, preserving any existing parameters.
 
-    :param delta: Timedelta for expiration.
-    :type delta: timedelta
-    :return: Datetime object representing the expiration time.
-    :rtype: datetime
+    :param response: HttpResponseRedirect object to be modified.
+    :param new_params: Dictionary of new query parameters to append.
     """
-    return datetime.utcnow() + delta
+    # Split the URL
+    url_parts = list(urlsplit(response["Location"]))
+    query = dict(parse_qs(url_parts[3]))
+
+    # Update with new query parameters
+    query.update(new_params)
+
+    # Reconstruct the URL
+    url_parts[3] = urlencode(query, doseq=True)
+    response["Location"] = urlunsplit(url_parts)
 
 
-def set_cookies(response: HttpResponse, user: Any) -> Tuple[int, Optional[int]]:
+def set_cookies(
+    response: HttpResponse, user: Any, transfer_timestamps: bool = True
+) -> None:
     """
     Set JWT and refresh token cookies on the HttpResponse object with CSRF rotation.
 
@@ -46,8 +59,8 @@ def set_cookies(response: HttpResponse, user: Any) -> Tuple[int, Optional[int]]:
     :type response: HttpResponse
     :param user: User instance for generating JWT and refresh tokens.
     :type user: Any
-    :return A tuple containing the expiration timestamps of the tokens.
-    :rtypei Tuple[int, Optional[int]]
+    :param transfer_timestamps: Whether to append the timestamps to the response querystring.
+    :type transfer_tyimestamps: bool
     """
     # JWT Token
     payload = jwt_payload(user)
@@ -71,8 +84,26 @@ def set_cookies(response: HttpResponse, user: Any) -> Tuple[int, Optional[int]]:
             refresh_token_instance.get_token(),
             refresh_expires,
         )
-        return (jwt_expires, refresh_expires)
-    return (jwt_expires, None)
+
+    if transfer_timestamps:
+        query_params = {
+            "jwt_expires": jwt_expires,
+        }
+        if jwt_settings.JWT_ALLOW_REFRESH:
+            query_params["refresh_expires"] = refresh_expires
+        append_query_params(response, query_params)
+
+
+def calculate_expiration(delta: timedelta) -> datetime:
+    """
+    Calculate the expiration time based on the provided timedelta.
+
+    :param delta: Timedelta for expiration.
+    :type delta: timedelta
+    :return: Datetime object representing the expiration time.
+    :rtype: datetime
+    """
+    return datetime.utcnow() + delta
 
 
 def is_refresh_token_expired(refresh_token: str, request: HttpRequest) -> bool:
